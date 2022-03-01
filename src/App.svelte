0afbf58e4930edeb6fe1373d5cda8dc9879e50d5
Akash Hasendra <akashhasendra2@gmail.com>
2022-03-01 12:23:00 +0530

    chat app

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<scriipt>
+   import Chat from './Chat.svelte';
+   import Header from './Header.svelte';
+</scriipt>
+
+<div class="app">
+    <Header/>
+    <Chat/>
+</div>
---
+++ src/Header.svelte
@@ -0,0 +1,13 @@
+<header>
+<h1>Chat App ✒️</h1>
+{#if $username}
+<div class="user-bio">
+    <span> Hello 👋 <strong>{$username}</strong></span>
+    <img src={'https://avatars.dicebear.com/api/human/${$username}'}.svg'} 
+</div>
+
+<button class="signout-button" on:click={signout}>Sign out</button>
+{:else}
+<h3>Chat App ⛓</h3>
+{/if}
+</header>
